47f2e2ed1e9365d2a76ddab2fab7588cea9bb42d
YHC <river@quseit.com>
2018-11-06 18:11:33 +0800

    Remove reactjs

--- reactjs/components/HelloFriend/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import markdown from '../../../README.md';
-const HelloFriend = () => (
-  <div className="hello">
-    	<div dangerouslySetInnerHTML={{ __html: markdown}}></div>
-  </div>
-);
-
-export default HelloFriend;
